3121df280667eb9e1bbd952a05d56348124b4af7
Peer Jakobsen <peerjako@amazon.com>
2023-05-21 22:03:06 +0200

    Added some first UI structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,85 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
+import { createPortal } from 'react-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  AppLayout,
+  ContentLayout,
+  Header,
+  Input,
+  TopNavigation
+} from '@cloudscape-design/components';
+ import "@cloudscape-design/global-styles/index.css"
+
+const i18nStrings = {
+  searchIconAriaLabel: 'Search',
+  searchDismissIconAriaLabel: 'Close search',
+  overflowMenuTriggerText: 'More',
+  overflowMenuTitleText: 'All',
+  overflowMenuBackIconAriaLabel: 'Back',
+  overflowMenuDismissIconAriaLabel: 'Close menu',
+};
+
+const Content = () => {
+  return (
+      <ContentLayout header={<Header variant="h1">Run simulation</Header>}>
+      </ContentLayout>
+    );
+  }
+
+
+interface PortalProps {
+  children: React.ReactNode;
+}
+
+const DocWriterHeaderPortal = ({ children }: PortalProps): JSX.Element => {
+  const domNode: HTMLElement | null = document.querySelector('#h');
+  if (!domNode) {
+    throw new Error('Could not find #h');
+  }
+  return createPortal(children, domNode);
+}
+
+function App() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  return (
+    <>
+      <DocWriterHeaderPortal>
+        <TopNavigation
+          i18nStrings={i18nStrings}
+          identity={{
+            href: '#',
+            title: 'Service name',
+            //logo: { src: logo, alt: 'Service name logo' },
+          }}
+          search={
+            <Input
+              ariaLabel="Input field"
+              clearAriaLabel="Clear"
+              value={searchValue}
+              type="search"
+              placeholder="Search"
+              onChange={({ detail }) => setSearchValue(detail.value)}
+            />
+          }
+        />
+      </DocWriterHeaderPortal>
+      <AppLayout
+        contentType="form"
+        content={Content()}
+        //breadcrumbs={<Breadcrumbs />}
+        //navigation={<Navigation activeHref="#/distributions" />}
+        toolsHide={true}
+      //notifications={<Notifications />}
+      />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('app') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
