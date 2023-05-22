7447573b9c21b7040b5136a43e88cf217aef7ee0
Peer Jakobsen <peerjako@amazon.com>
2023-05-22 22:07:33 +0200

    Restructuring navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {
+  Input,
+  TopNavigation
+} from '@cloudscape-design/components';
+import PRFAQPage from './pages/PRFAQ/PRFAQPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const i18nStrings = {
+  searchIconAriaLabel: 'Search',
+  searchDismissIconAriaLabel: 'Close search',
+  overflowMenuTriggerText: 'More',
+  overflowMenuTitleText: 'All',
+  overflowMenuBackIconAriaLabel: 'Back',
+  overflowMenuDismissIconAriaLabel: 'Close menu',
+};
+
+interface PortalProps {
+  children: React.ReactNode;
 }
 
+const DocWriterHeaderPortal = ({ children }: PortalProps): JSX.Element => {
+  const domNode: HTMLElement | null = document.querySelector('#h');
+  if (!domNode) {
+    throw new Error('Could not find #h');
+  }
+  return createPortal(children, domNode);
+}
+
+function App() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const[pageName, setPageName]=useState('Default page name');
+  return (
+    <>
+      <DocWriterHeaderPortal>
+        <TopNavigation
+          i18nStrings={i18nStrings}
+          identity={{
+            href: '#',
+            title: pageName,
+            //logo: { src: logo, alt: 'Service name logo' },
+          }}
+          search={
+            <Input
+              ariaLabel="Input field"
+              clearAriaLabel="Clear"
+              value={searchValue}
+              type="search"
+              placeholder="Search"
+              onChange={({ detail }) => setSearchValue(detail.value)}
+            />
+          }
+        />
+      </DocWriterHeaderPortal>
+      <Router>
+        <Routes>
+          <Route path="/prfaq" element={<PRFAQPage setPageName={setPageName} />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
 export default App;
